5c0c8cc28e0a5246616bf159537158fe97b63a6f
Aron Phong <aronphong@gmail.com>
2020-09-19 16:50:25 -0700

    Create post detail routes

--- controllers/postController.js
+++ controllers/postController.js
@@ -4,7 +4,7 @@ const User = require("../models/User");
 // @route  GET /timeline
 // @desc   Get all posts
 // @access Private
-exports.timeline_posts_get = async (req, res) => {
+exports.posts_get = async (req, res) => {
   try {
     const posts = await Post.find({}).sort({ date: -1 });
     res.json(posts);
@@ -17,7 +17,7 @@ exports.timeline_posts_get = async (req, res) => {
 // @route  POST /timeline
 // @desc   Get all posts
 // @access Private
-exports.timeline_posts_post = async (req, res) => {
+exports.posts_post = async (req, res) => {
   try {
     const user = await User.findById(req.user.id).select("-password");
 
@@ -36,10 +36,58 @@ exports.timeline_posts_post = async (req, res) => {
   }
 };
 
-// @todo   Get posts by ID
 // @route  GET /timeline/post/:id
 // @desc   Get post by ID
 // @access Private
+exports.post_detail_get = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      res.status(404).json({ msg: "Post not found" });
+    }
+
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+
+    if (err.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+  }
+};
+
+// @route  DELETE /timeline/post/:id
+// @desc   Delete post by ID
+// @access Private
+exports.post_detail_delete = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      res.status(404).json({ msg: "Post not found" });
+    }
+
+    // check user
+    if (post.user.toString() !== req.user.id) {
+      return res
+        .status(401)
+        .json({ msg: "User not authorized to delete post" });
+    }
+
+    await post.remove();
+
+    res.json({ msg: "Post removed" });
+  } catch (err) {
+    console.error(err.message);
+
+    if (err.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+
+    res.status(500).send("Server Error");
+  }
+};
 
 // @todo   Like a post by ID
 // @route  PUT /timeline/like/:id
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -4,23 +4,27 @@ const auth = require("../../middleware/auth");
 
 // require controller modules
 const {
-  timeline_posts_get,
-  timeline_posts_post,
+  posts_get,
+  posts_post,
+  post_detail_get,
 } = require("../../controllers/postController");
 
 // require validator modules
 const { validate_post } = require("../../validators/postvalidator");
 
-// require models
-const Post = require("../../models/Post");
-
 // GET all posts
-router.get("/timeline", [auth], timeline_posts_get);
+router.get("/", [auth], posts_get);
 
 // POST create post
-router.post("/timeline", [auth, validate_post], timeline_posts_post);
+router.post("/", [auth, validate_post], posts_post);
+
+// Get post by ID
+router.get("/post/:id", [auth], post_detail_get);
 
 // DELETE delete post
+router.delete("/post/:id", [auth], post_detail_delete);
+
+// @todo
 // PUT like post
 // PUT unlike post
 // PUT comment post

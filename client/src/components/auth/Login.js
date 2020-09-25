d25357d5a5d1476c60a81df26a58ebe08d1792f3
Aron Phong <aronphong@gmail.com>
2020-09-25 07:11:48 -0700

    Create user login component

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,73 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
+import { connect } from "react-redux";
+import { Link, Redirect } from "react-router-dom";
+import { login } from "../../actions/auth";
+import PropTypes from "prop-types";
 
-const Login = () => {
-  return <div>Login</div>;
+const Login = ({ login, isAuthenticated }) => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formData;
+
+  const onChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async (e) => {
+    // Prevent browser refresh
+    e.preventDefault();
+    login(email, password);
+  };
+
+  // Redirect if valid credentials
+  if (isAuthenticated) {
+    return <Redirect to='/' />;
+  }
+  return (
+    <Fragment>
+      <h1>Sign In</h1>
+      <p>Log in to your account</p>
+      <form className='form' onSubmit={onSubmit}>
+        <div>
+          <input
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            value={email}
+            onChange={onChange}
+            required
+          />
+        </div>
+        <div>
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            value={password}
+            onChange={onChange}
+            required
+          />
+        </div>
+
+        <input type='submit' className='btn' value='Register'></input>
+      </form>
+      <p>
+        Don't have an account? <Link to='/register'>Sign Up</Link>
+      </p>
+    </Fragment>
+  );
 };
 
-export default Login;
+Login.propTypes = {
+  login: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool,
+};
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+});
+
+export default connect(mapStateToProps, { login })(Login);

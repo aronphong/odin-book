25337effc2235b0cb0717db3b16b8272d133a93e
Aron Phong <aronphong@gmail.com>
2020-09-26 13:35:10 -0700

    Provide user token to all routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
@@ -6,13 +6,30 @@ import Routes from "./components/routing/Routes";
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
+import { loadUser } from "./actions/auth";
+import { LOGOUT } from "./actions/types";
+import { setAuthToken } from "./utils/setAuthToken";
 
 import "./Reset.css";
 import "./App.css";
 
-const App = () => (
-  <Provider store={store}>
-    <Fragment>
+const App = () => {
+  useEffect(() => {
+    // check for token in LS
+    if (localStorage.token) {
+      setAuthToken(localStorage.token);
+    }
+
+    store.dispatch(loadUser());
+
+    // logout from all tabs if user logs out
+    window.addEventListener("storage", () => {
+      if (!localStorage.token) store.dispatch({ type: LOGOUT });
+    });
+  }, []);
+
+  return (
+    <Provider store={store}>
       <Router>
         <Fragment>
           <Navbar />
@@ -22,8 +39,7 @@ const App = () => (
           </Switch>
         </Fragment>
       </Router>
-    </Fragment>
-  </Provider>
-);
-
+    </Provider>
+  );
+};
 export default App;

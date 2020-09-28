bf1a713437e18a7abc5df61ebadf360d5e59a7c0
Aron Phong <aronphong@gmail.com>
2020-09-28 07:02:25 -0700

    Create ADD_POST reducer

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,4 +1,4 @@
-import { GET_POSTS, POST_ERROR } from "../actions/types";
+import { ADD_POST, GET_POSTS, POST_ERROR } from "../actions/types";
 
 const initialState = {
   posts: [],
@@ -15,6 +15,13 @@ export default function (state = initialState, action) {
       return {
         ...state,
         posts: payload,
+        loading: false,
+      };
+
+    case ADD_POST:
+      return {
+        ...state,
+        posts: [payload, ...state.posts],
         loading: false,
       };
 

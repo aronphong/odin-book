e036103b3162c6582e0207fb6deaf47bfe14bfa8
Aron Phong <aronphong@gmail.com>
2020-09-28 07:01:43 -0700

    Create post form component

---
+++ client/src/components/posts/PostForm.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { addPost } from "../../actions/post";
+
+const PostForm = ({ addPost }) => {
+  const [text, setText] = useState("");
+
+  return (
+    <div className='post-form'>
+      <div>
+        <h3>Whats on your mind?</h3>
+      </div>
+      <form
+        className='post-form'
+        onSubmit={(e) => {
+          e.preventDefault();
+          addPost({ text });
+          setText("");
+        }}
+      >
+        <textarea
+          name='text'
+          cols='30'
+          rows='5'
+          placeholder='Create a post'
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          required
+        />
+        <input type='submit' className='btn' value='Submit' />
+      </form>
+    </div>
+  );
+};
+
+PostForm.propTypes = {
+  addPost: PropTypes.func.isRequired,
+};
+
+export default connect(null, { addPost })(PostForm);

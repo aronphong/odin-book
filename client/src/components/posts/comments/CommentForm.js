3d98701291a13a71d760b5c858a9e8f24a81bb7b
Aron Phong <aronphong@gmail.com>
2020-10-07 06:47:04 -0700

    Update class names and remove submit button

--- client/src/components/posts/comments/CommentForm.js
+++ client/src/components/posts/comments/CommentForm.js
@@ -6,24 +6,23 @@ import { addComment } from "../../../actions/post";
 const CommentForm = ({ postId, addComment }) => {
   const [text, setText] = useState("");
   return (
-    <form
-      className='comment-form'
-      onSubmit={(e) => {
-        e.preventDefault();
-        addComment(postId, { text });
-        setText("");
-      }}
-    >
-      <textarea
-        name='commentText'
-        cols='30'
-        rows='3'
-        placeholder='Add a comment'
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-      />
-      <input type='submit' className='btn' value='Submit' />
-    </form>
+    <div className='comment-form'>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          addComment(postId, { text });
+          setText("");
+        }}
+      >
+        <input
+          type='text'
+          name='commentText'
+          placeholder='Write a comment...'
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+      </form>
+    </div>
   );
 };
 

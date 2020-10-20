6c32902609791b4b6e1ff245645b5d806cb3b9a8
Aron Phong <aronphong@gmail.com>
2020-10-20 06:26:59 -0700

    Fix objectId failure bug

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,4 @@
 const express = require("express");
-
 const router = express.Router();
 const auth = require("../../middleware/auth");
 
@@ -16,14 +15,14 @@ const {
 // GET current user profile
 router.get("/me", [auth], my_profile_get);
 
-// GET specific user profile
-router.get("/:id", [auth], profile_get);
-
 // GET all profiles
 router.get("/profiles", [auth], profiles_get);
 
 // DELETE profile, user & posts
 router.delete("/me", [auth], profile_delete);
+
+// GET specific user profile
+router.get("/:id", [auth], profile_get);
 
 /* @todo
     Update user profile
